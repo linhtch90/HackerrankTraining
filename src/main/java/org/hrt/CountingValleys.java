598c872c3cc6311f6f2a3365c757bd342f136293
Boris Truong <linhtch90@gmail.com>
2026-05-07 00:07:10 +0700

    Counting Valleys

---
+++ src/main/java/org/hrt/CountingValleys.java
@@ -0,0 +1,63 @@
+package org.hrt;
+
+import java.io.*;
+
+class Result {
+
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER steps
+     *  2. STRING path
+     */
+
+    public static int countingValleys(int steps, String path) {
+        // Write your code here
+
+        int valleyCount = 0;
+        boolean inValley = false;
+        int lat = 0;
+        for (int i = 0; i < steps; i++) {
+            if (path.charAt(i) == 'U') {
+                lat++;
+            } else {
+                lat--;
+            }
+
+            if (lat < 0) {
+                inValley = true;
+            } else {
+                if (inValley) {
+                    valleyCount++;
+                }
+                inValley = false;
+            }
+        }
+
+        return valleyCount;
+
+    }
+
+}
+
+public class CountingValleys {
+    static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int steps = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String path = bufferedReader.readLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
