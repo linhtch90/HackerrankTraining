76195cd3951d7e781a86948fce6500759249dfbe
Boris Truong <linhtch90@gmail.com>
2026-05-07 00:18:27 +0700

    SalesByMatch

---
+++ src/main/java/org/hrt/SalesByMatch.java
@@ -0,0 +1,61 @@
+package org.hrt;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<Integer> ar) {
+        // Write your code here
+
+        Map<Integer, Integer> visited = new HashMap<>();
+        for (int i : ar) {
+            visited.merge(i, 1, Integer::sum);
+        }
+
+        int result = 0;
+        for (int i : visited.values()) {
+            result += i / 2;
+        }
+
+        return result;
+
+    }
+
+}
+
+public class SalesByMatch {
+    static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
